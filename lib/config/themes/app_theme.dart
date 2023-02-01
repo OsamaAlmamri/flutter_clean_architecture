ac98818e4001daee4a807f35367260d8b9420e3f
Osama Al-mamari <yemencoder@gmail.com>
2023-02-01 18:16:15 +0300

    fonts

--- lib/config/themes/app_theme.dart
+++ lib/config/themes/app_theme.dart
@@ -1,16 +1,15 @@
 import 'package:clean_architecture/core/utils/app_colors.dart';
+import 'package:clean_architecture/core/utils/app_strings.dart';
 import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
   return ThemeData(
-    primaryColor: AppColors.primary,
-    hintColor: AppColors.hints,
-    brightness: Brightness.light,
-    scaffoldBackgroundColor: Colors.white,
-    textTheme: TextTheme(button: TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w500,
-      fontSize: 16
-    ))
-  );
+      primaryColor: AppColors.primary,
+      hintColor: AppColors.hints,
+      brightness: Brightness.light,
+      scaffoldBackgroundColor: Colors.white,
+      fontFamily: AppString.fontFamily,
+      textTheme: TextTheme(
+          button: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.w500, fontSize: 16)));
 }
--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,3 +1,4 @@
 class AppString{
   static const String appName="Quotes App";
+  static const String fontFamily = 'Tajawal';
 }
