098bbb2876db1690e149a9cea4296621ee77f1f0
Osama Al-mamari <yemencoder@gmail.com>
2023-02-01 18:05:04 +0300

    HexColor

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,8 @@ import 'package:clean_architecture/core/utils/app_strings.dart';
 import 'package:clean_architecture/core/utils/app_colors.dart';
 import 'package:clean_architecture/features/random_quote/presentation/screens/quote_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'core/utils/hex_color.dart';
 class QuoteApp extends StatelessWidget {
   const QuoteApp({Key? key}) : super(key: key);
 
@@ -10,9 +12,8 @@ class QuoteApp extends StatelessWidget {
     return MaterialApp(
       title: AppString.appName,
       debugShowCheckedModeBanner: false,
-      theme: const ThemeData(
-        primaryColor: AppColors.primary,
-      ),
+      theme:  ThemeData(
+        primaryColor:HexColor("#0000FF"),),
 
       home: const QuoteScreen(),
     );
