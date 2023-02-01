b44632c1255e9da5178203df9f59daac4ec559bc
Osama Al-mamari <yemencoder@gmail.com>
2023-02-01 19:15:24 +0300

    Media Query

--- lib/features/random_quote/presentation/screens/quote_screen.dart
+++ lib/features/random_quote/presentation/screens/quote_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clean_architecture/core/utils/app_strings.dart';
 import 'package:clean_architecture/core/utils/assets_manager.dart';
 import 'package:clean_architecture/core/utils/constants.dart';
+import 'package:clean_architecture/core/utils/media_queries_values.dart';
 import 'package:flutter/material.dart';
 
 class QuoteScreen extends StatelessWidget {
@@ -17,7 +18,7 @@ class QuoteScreen extends StatelessWidget {
           children: [
             ElevatedButton(onPressed:()=> Constants.showErrorDialog(context: context, msg: "error"), child: Text("show error")),
             ElevatedButton(onPressed:()=> Constants.showToast( msg: "error"), child: Text("show toast")),
-            Image.asset(ImgAssets.quote),
+            Image.asset(ImgAssets.quote,width:context.width,),
           ],
         ),
       ),
