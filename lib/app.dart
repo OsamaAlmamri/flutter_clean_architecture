51225dc1a4d06916ab7f05d1d66849fa3f972789
Osama Al-mamari <yemencoder@gmail.com>
2023-02-01 18:00:52 +0300

    app colors

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:clean_architecture/core/utils/app_strings.dart';
+import 'package:clean_architecture/core/utils/app_colors.dart';
 import 'package:clean_architecture/features/random_quote/presentation/screens/quote_screen.dart';
 import 'package:flutter/material.dart';
 class QuoteApp extends StatelessWidget {
@@ -9,8 +10,8 @@ class QuoteApp extends StatelessWidget {
     return MaterialApp(
       title: AppString.appName,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: const ThemeData(
+        primaryColor: AppColors.primary,
       ),
 
       home: const QuoteScreen(),
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class AppColors{
+
+  static const Color primary=Colors.blue;
+  static const Color hints=Colors.grey;
+}
