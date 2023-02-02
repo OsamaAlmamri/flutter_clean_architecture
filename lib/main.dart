8c9e37dc5a8fecf5b48edb5db5bfe5c122ad76c6
Osama Al-mamari <yemencoder@gmail.com>
2023-02-02 19:55:28 +0300

    presentation - cubit logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:clean_architecture/app.dart';
+import 'package:clean_architecture/bloc_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const QuoteApp());
+  BlocOverrides.runZoned(
+        () {
+      runApp(const QuoteApp());
+    },
+    blocObserver: AppBlocObserver(),
+  );
 }
 
 
