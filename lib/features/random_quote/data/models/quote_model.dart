c7aa10a8afa087e6b081cb05894fc2e06fad5542
Osama Al-mamari <yemencoder@gmail.com>
2023-02-02 19:10:45 +0300

    Data layer - Repository , DataSource

--- lib/features/random_quote/data/models/quote_model.dart
+++ lib/features/random_quote/data/models/quote_model.dart
@@ -2,16 +2,13 @@ import 'package:clean_architecture/features/random_quote/domain/entities/quote.d
 
 class QuoteModel extends Quote {
   const QuoteModel(
-      {required String author,
-      required int id,
-      required String content})
+      {required String author, required int id, required String content})
       : super(author: author, id: id, content: content);
 
   factory QuoteModel.fromJson(Map<String, dynamic> json) => QuoteModel(
         author: json["author"],
         id: json["id"],
         content: json["quote"],
-
       );
 
   Map<String, dynamic> toJson() => {
